30b53b3e5926e4442942b0d9f550cec1f90fede2
flaviopantaleo <flavio.pantaleo@yahoo.com>
2024-07-30 09:25:51 +0200

    add tag model with relationship with jobs, tag controller with view return and his route

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\SearchController;
+use App\Http\Controllers\TagController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\RegisteredUserController;
 
 Route::get('/', [JobController::class, 'index']);
 Route::get('/search', SearchController::class);
+Route::get('/tags/{tag:name}', TagController::class);
 
 // Auth
 
