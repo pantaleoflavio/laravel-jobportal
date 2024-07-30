7d3be4470d71745d5bdf02cb44b4c2311bf1024d
flaviopantaleo <flavio.pantaleo@yahoo.com>
2024-07-30 10:08:04 +0200

    fix small FE issues

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -7,7 +7,11 @@
 
     <div class="flex-1 flex flex-col">
         <a href="" class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
-        <h4 class="font-bold text-xl mt-3 group-hover:text-jobCardBorder transition-colors duration-700">{{ $job->title }}</h4>
+        <h4 class="font-bold text-xl mt-3 group-hover:text-jobCardBorder transition-colors duration-700">
+            <a href="{{ $job->url }}">
+                {{ $job->title }}
+            </a>
+        </h4>
         <p class="text-sm text-gray-400 mt-auto">{{ $job->schedule }} - {{ $job->salary }}</p>
     </div>
     <div class="">
