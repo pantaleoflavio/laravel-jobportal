c749b5f63b9d1864ce75701e5a74b7da67fff784
flaviopantaleo <flavio.pantaleo@yahoo.com>
2024-07-31 17:25:02 +0200

    add back button to view show job

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,7 +3,8 @@
     <x-page-heading>{{ $job->title }}</x-page-heading>
     <x-forms.divider></x-forms.divider>
     <x-job-card-wide :$job />
-    <div class="mt-6">
+    <div class="mt-6 flex justify-between">
         <x-anchor href="/jobs/{{$job->id}}/edit" aType="edit">Edit Job</x-anchor>
+        <x-anchor href="/">back</x-anchor>
     </div>
 </x-layout>
